adf80a564d726dc4205fa40af6c5edcf32532808
Guillaume Marolleau <gmarolleau@gmail.com>
2025-02-05 17:53:27 +0100

    4.0.2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let vlcBinary = Target.binaryTarget(name: "VLCKit-all", url: "https://github.com/g-mrll/hl-vlckit-ios-spm/releases/download/v1.0.0/VLCKit-all.xcframework.zip", checksum: "34c1cebb466d59ad3793ed02aa46a5d0e4c210e257cb97de07540d6f0ca71086")
+let vlcBinary = Target.binaryTarget(name: "VLCKit", url: "https://github.com/g-mrll/hl-vlckit-ios-spm/releases/download/v4.0.1/VLCKit.xcframework.zip", checksum: "c68bf70b34db8242cd8b1bbce2eea89671a5e821623ee0c5f7c84e9a56cc0499")
 
 let package = Package(
     name: "vlckit-spm",
--- generate.sh
+++ generate.sh
@@ -1,49 +1,24 @@
 #!/bin/sh
 #
+
+_TAG_VERSION="${1}";
+
+IOS_URL="https://download.videolan.org/pub/cocoapods/unstable/VLCKit-4.0.0a9-a3480636-ba880a0b.tar.xz"
+
 rm -rf .tmp/ || true
-
-IOS_URL="https://download.videolan.org/pub/cocoapods/unstable/MobileVLCKit-3.6.1b1-8e652244-ac310b4b.tar.xz"
-MACOS_URL="https://download.videolan.org/pub/cocoapods/unstable/VLCKit-3.6.0b10-615f96dc-4733d1cc.tar.xz"
-TVOS_URL="https://download.videolan.org/cocoapods/unstable/TVVLCKit-3.6.0b10-615f96dc-4733d1cc.tar.xz"
-
 mkdir .tmp/
 
-#Download and generate MobileVLCKit
-wget -O .tmp/MobileVLCKit.tar.xz $IOS_URL
-tar -xf .tmp/MobileVLCKit.tar.xz -C .tmp/
-
-#Download and generate VLCKit
-wget -O .tmp/VLCKit.tar.xz $MACOS_URL
+wget -O .tmp/VLCKit.tar.xz $IOS_URL
 tar -xf .tmp/VLCKit.tar.xz -C .tmp/
 
-#Download and generate TVVLCKit
-wget -O .tmp/TVVLCKit.tar.xz $TVOS_URL
-tar -xf .tmp/TVVLCKit.tar.xz -C .tmp/
+IOS_LOCATION=".tmp/VLCKit-binary/VLCKit.xcframework"
 
-IOS_LOCATION=".tmp/MobileVLCKit-binary/MobileVLCKit.xcframework"
-TVOS_LOCATION=".tmp/TVVLCKit-binary/TVVLCKit.xcframework"
-MACOS_LOCATION=".tmp/VLCKit - binary package/VLCKit.xcframework"
-
-#Merge into one xcframework
-xcodebuild -create-xcframework \
-    -framework "$MACOS_LOCATION/macos-arm64_x86_64/VLCKit.framework" \
-    -debug-symbols "${PWD}/$MACOS_LOCATION/macos-arm64_x86_64/dSYMs/VLCKit.framework.dSYM" \
-    -framework "$TVOS_LOCATION/tvos-arm64_x86_64-simulator/TVVLCKit.framework" \
-    -debug-symbols "${PWD}/$TVOS_LOCATION/tvos-arm64_x86_64-simulator/dSYMs/TVVLCKit.framework.dSYM" \
-    -framework "$TVOS_LOCATION/tvos-arm64/TVVLCKit.framework"  \
-    -debug-symbols "${PWD}/$TVOS_LOCATION/tvos-arm64/dSYMs/TVVLCKit.framework.dSYM" \
-    -framework "$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/MobileVLCKit.framework" \
-    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_i386_x86_64-simulator/dSYMs/MobileVLCKit.framework.dSYM" \
-    -framework "$IOS_LOCATION/ios-arm64_armv7_armv7s/MobileVLCKit.framework" \
-    -debug-symbols "${PWD}/$IOS_LOCATION/ios-arm64_armv7_armv7s/dSYMs/MobileVLCKit.framework.dSYM" \
-    -output .tmp/VLCKit-all.xcframework
-    
-ditto -c -k --sequesterRsrc --keepParent ".tmp/VLCKit-all.xcframework" ".tmp/VLCKit-all.xcframework.zip"
+ditto -c -k --sequesterRsrc --keepParent "${IOS_LOCATION}" "${IOS_LOCATION}.zip"
 
 #Update package file
-PACKAGE_HASH=$(sha256sum ".tmp/VLCKit-all.xcframework.zip" | awk '{ print $1 }')
-PACKAGE_STRING="Target.binaryTarget(name: \"VLCKit-all\", url: \"https:\/\/github.com\/g-mrll\/hl-vlckit-ios-spm\/releases\/download\/v1.0.0\/VLCKit-all.xcframework.zip\", checksum: \"$PACKAGE_HASH\")"
+PACKAGE_HASH=$(sha256sum "${IOS_LOCATION}.zip" | awk '{ print $1 }')
+PACKAGE_STRING="Target.binaryTarget(name: \"VLCKit\", url: \"https:\/\/github.com\/g-mrll\/hl-vlckit-ios-spm\/releases\/download\/v${_TAG_VERSION}\/VLCKit.xcframework.zip\", checksum: \"$PACKAGE_HASH\")"
 echo "Changing package definition for xcframework with hash $PACKAGE_HASH"
 sed -i '' -e "s/let vlcBinary.*/let vlcBinary = $PACKAGE_STRING/" Package.swift
 
-cp -f .tmp/MobileVLCKit-binary/COPYING.txt ./LICENSE
+cp -f .tmp/VLCKit-binary/COPYING.txt ./LICENSE
