31d45c42b21282a950478637662fd00ea661edb2
Guillaume Marolleau <gmarolleau@gmail.com>
2025-02-05 18:05:43 +0100

    4.0.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let vlcBinary = Target.binaryTarget(name: "VLCKit", url: "https://github.com/g-mrll/hl-vlckit-ios-spm/releases/download/v4.0.2/VLCKit.xcframework.zip", checksum: "e53fb9f7d59e54dd6d094aee0344ac762aad4ce01d08be963c19cdc018967968")
+let vlcBinary = Target.binaryTarget(name: "VLCKit", url: "https://github.com/g-mrll/hl-vlckit-ios-spm/releases/download/v4.0.3/VLCKit.xcframework.zip", checksum: "e53fb9f7d59e54dd6d094aee0344ac762aad4ce01d08be963c19cdc018967968")
 
 let package = Package(
     name: "vlckit-spm",
@@ -19,7 +19,7 @@ let package = Package(
         .target(
             name: "VLCKitSPM",
             dependencies: [
-                .target(name: "VLCKit-all")
+                .target(name: "VLCKit")
             ], linkerSettings: [
                 .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
                 .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
